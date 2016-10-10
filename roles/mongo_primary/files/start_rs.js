2a4da100d71a3f6e7acc92295e45e0d516393929
emerson <emerson@epplkraw0166t1.princeton.epam.com>
2016-10-10 15:14:37 -0500

    starts and loads the replica set

---
+++ roles/mongo_primary/files/start_rs.js
@@ -0,0 +1,19 @@
+db2 = (new Mongo('localhost:27017')).getDB('test')
+config = {
+  	"_id" : "rs0",
+  	"members" : [
+  		{
+  			"_id" : 0,
+  			"host" : "192.168.56.105:27017"
+  		},
+  		{
+  			"_id" : 1,
+  			"host" : "192.168.56.103:27017"
+  		},
+  		{
+  			"_id" : 2,
+  			"host" : "192.168.56.101:27017"
+  		},
+	]
+}
+rs.initiate(config)
